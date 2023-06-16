a2dc5c2fc5d2a9c3fa4ef62e22c92da583a6f6f4
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-06-16 20:13:21 +0300

    Add oobabooga model settings

--- src/complete/completers/oobabooga/oobabooga.tsx
+++ src/complete/completers/oobabooga/oobabooga.tsx
@@ -9,16 +9,100 @@ import SettingsItem from "../../../components/SettingsItem";
 import { z } from "zod";
 
 export const model_settings_schema = z.object({
-	context_length: z.number().int().positive(),
+	context_length: z.number().int().nonnegative(),
+	max_new_tokens: z.number().int().nonnegative().optional(),
+	temperature: z.number().nonnegative().optional(),
+	top_p: z.number().nonnegative().optional(),
+	typical_p: z.number().nonnegative().optional(),
+	top_k: z.number().int().nonnegative().optional(),
+	min_length: z.number().int().nonnegative().optional(),
+	penalty_alpha: z.number().nonnegative().optional(),
+	repetition_penalty: z.number().nonnegative().optional(),
+	length_penalty: z.number().nonnegative().optional(),
+	no_repeat_ngram_size: z.number().int().nonnegative().optional(),
 });
+
+const default_model_settings: ModelSettings = {
+	context_length: 4000,
+	max_new_tokens: 120,
+	temperature: 0.3,
+	top_p: 1,
+	typical_p: 1,
+	top_k: 0,
+	min_length: 0,
+	penalty_alpha: 0,
+	repetition_penalty: 1.18,
+	length_penalty: 1,
+	no_repeat_ngram_size: 0,
+};
+const model_settings_fields: {
+	name: string;
+	description: string;
+	field_name: keyof ModelSettings;
+}[] = [
+	{
+		name: "Context length",
+		description: "In characters, how much context should the model get",
+		field_name: "context_length",
+	},
+	{
+		name: "Max new tokens",
+		description: "In tokens, how many tokens should the model generate",
+		field_name: "max_new_tokens",
+	},
+	{
+		name: "Temperature",
+		description: "How creative should the model be",
+		field_name: "temperature",
+	},
+	{
+		name: "Top p",
+		description: "What percentage of tokens should be considered",
+		field_name: "top_p",
+	},
+	{
+		name: "Typical p",
+		description: "Cumulative probability of considered tokens",
+		field_name: "typical_p",
+	},
+	{
+		name: "Top k",
+		description: "How many tokens should be considered (0 = all)",
+		field_name: "top_k",
+	},
+	{
+		name: "Min length",
+		description: "Minimum length of generated text (in tokens)",
+		field_name: "min_length",
+	},
+	{
+		name: "Penalty alpha",
+		description: "Penalty alpha",
+		field_name: "penalty_alpha",
+	},
+	{
+		name: "Repetition penalty",
+		description: "How much should the model avoid repeating itself",
+		field_name: "repetition_penalty",
+	},
+	{
+		name: "Length penalty",
+		description: "How much should the model avoid short outputs",
+		field_name: "length_penalty",
+	},
+	{
+		name: "No repeat ngram size",
+		description: "How long are the sequences that should be kept unique",
+		field_name: "no_repeat_ngram_size",
+	},
+];
+
 export type ModelSettings = z.infer<typeof model_settings_schema>;
 const parse_model_settings = (settings: string): ModelSettings => {
 	try {
 		return model_settings_schema.parse(JSON.parse(settings));
 	} catch (e) {
-		return {
-			context_length: 4000,
-		};
+		return { context_length: 4000 };
 	}
 };
 
@@ -42,24 +126,37 @@ export default class OobaboogaModel implements Model {
 	}: {
 		settings: string | null;
 		saveSettings: (settings: string) => void;
-	}) => (
-		<SettingsItem
-			name="Context length"
-			description="In characters, how much context should the model get"
-		>
-			<input
-				type="number"
-				value={parse_model_settings(settings || "").context_length}
-				onChange={(e) =>
-					saveSettings(
-						JSON.stringify({
-							context_length: parseInt(e.target.value),
-						})
-					)
-				}
-			/>
-		</SettingsItem>
-	);
+	}) => {
+		const parsed_settings = parse_model_settings(settings || "");
+		return (
+			<>
+				{model_settings_fields.map((property) => (
+					<SettingsItem
+						name={property.name}
+						description={property.description}
+					>
+						<input
+							type="number"
+							value={parsed_settings[property.field_name]}
+							placeholder={default_model_settings[
+								property.field_name
+							]?.toString()}
+							onChange={(e) =>
+								saveSettings(
+									JSON.stringify({
+										...parsed_settings,
+										[property.field_name]: parseFloat(
+											e.target.value
+										),
+									})
+								)
+							}
+						/>
+					</SettingsItem>
+				))}
+			</>
+		);
+	};
 
 	constructor(id: string, provider_settings: string) {
 		this.id = id;
@@ -126,25 +223,16 @@ export default class OobaboogaModel implements Model {
 
 	async complete(prompt: Prompt, settings: string): Promise<string> {
 		const parsed_settings = parse_model_settings(settings);
+		const { context_length, ...model_params } = parsed_settings;
 
 		const response = await this.create_completion({
-			prompt: prompt.prefix.slice(-parsed_settings.context_length),
-			max_new_tokens: 120,
+			...model_params,
+			prompt: prompt.prefix.slice(-context_length),
 			do_sample: true,
-			temperature: 1.3,
-			top_p: 0.1,
-			typical_p: 1,
-			repetition_penalty: 1.18,
-			top_k: 40,
-			min_length: 0,
-			no_repeat_ngram_size: 0,
 			num_beams: 1,
-			penalty_alpha: 0,
-			length_penalty: 1,
 			early_stopping: false,
 			seed: -1,
 			add_bos_token: true,
-			truncation_length: 2048,
 			ban_eos_token: false,
 			skip_special_tokens: true,
 			stopping_strings: [],
